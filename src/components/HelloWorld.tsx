056cc9ae0ee7d7df2d357c67ee2772cb511a7a4f
paul <spammer4@planetsugar.co.uk>
2019-01-01 15:07:14 +0000

    Added in css and image processing

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as constants from "@app/constants";
+import "@app/styles/main.css";
 
 export interface IHelloWorldProps {
   compiler: string;
@@ -15,6 +16,9 @@ export class HelloWorld extends React.Component<IHelloWorldProps, {}> {
         </h1>
         <p>This means that the config is working.</p>
         <p>{constants.DEFAULT_GREETING}</p>
+        <div className="greentext">
+          This text should be green and be brought in from the style sheet.
+        </div>
       </React.Fragment>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ const plugins = (env) => {
         // Copy any static resources required to the dist folder  
         new CopyWebpackPlugin([
             { from: "src/images", to: "images" }
-        ]),       
+        ]) 
     ]);
 
     // If we are not doing any analysis on the bundle then we don't need this. 
@@ -91,16 +91,36 @@ module.exports = env => {
                 ]
             },
 
-            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
             // Exports HTML as a string and can minimise it, negates the use of CopyWebpackPlugin for 
             // index.html 
-            {   test: /\.html$/, use: [ { loader: "html-loader", options: { minimize: true } } ] }
+            {   test: /\.html$/, use: [ { loader: "html-loader", options: { minimize: true } } ] },
+
+            // css-loader turns the css into a string and the style loader put the style tag in the page. Styles will be included in the bundle,
+            // this is not efficient as they could be loaded async while the page is loading. Use extract-text-plugin to extract it out. 
+            { test: /\.css$/, use: [ {loader: "style-loader"}, {loader: "css-loader" }] },
+
+            // File loader will process png, gif and jpegs and output them to the dist folder 
+            { test: /\.(png|jpg|gif)$/, use: [ { loader: "file-loader"} ] },
+            
+            // typings-for-css-loader enabled type generation for style sheets, NOTE: Breaking changes after 1.0.0 on css-loader
+
+            //{ test: /\.css$/, use: [ { loader: "style-loader" }, { loader: "typings-for-css-modules-loader", options: { modules: true, namedExport: true, camelCase: true } } ] }            
         ]
     },   
     plugins: plugins(env)
 }
 };
+
+/* 
+    Notes:
+
+    style-loader is a Webpack loader that can load some CSS and inject it into the document via a <link> tag.
+    css-loader is the loader that can parse a CSS file and apply various transforms to it. 
+    extract-text-plugin can combine all the css output into one file 
+     
+*/
